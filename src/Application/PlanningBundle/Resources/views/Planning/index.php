391cf4f5a33c3ff11d91f97abded6fac55bcdcc2
Kévin Dunglas <dunglas@MacBook-de-Kevin-Dunglas.local>
2010-10-08 14:22:33 +0200

    Remove submodules

--- src/Application/PlanningBundle/Resources/views/Planning/index.php
+++ src/Application/PlanningBundle/Resources/views/Planning/index.php
@@ -1,1 +1,37 @@
+<?php $view->extend('PlanningBundle::layout.php') ?>
+
 Hello <?php echo $name ?>!
+
+/**
+
+*/
+
+// nfjkdsbnf
+
+# Commentaire
+
+classe : MaClasse
+méthode : texteEnGras()
+
+function : ma_function()
+p
+$_
+
+<?php
+$tab = array ('a', 'b', 'c');
+
+$tab[0] = 'a';
+$tab[1] = 'b';
+$tab[2] = 'c';
+
+//
+
+$tab[0][0] = 'a';
+
+//
+
+$tab_assoc = array ('fleur' => 'rose', 'couleur' => 'rouge');
+
+$tab_assoc['fleur'] = 'rose';
+$tab_assoc['couleur'] = 'rouge';
+
